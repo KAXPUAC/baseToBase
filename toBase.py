33dfe64de12ec8cbb5266ac2174baadb48af40e0
Kevin Axpuac <kevinjuarez@galileo.edu>
2020-04-28 23:27:43 -0600

    Convertidor de base x a base y

---
+++ toBase.py
@@ -0,0 +1,9 @@
+def toBase(numero: int, base: int):
+    chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if numero < 0:
+        numero = numero * -1
+    result = ''
+    while numero > 0:
+        numero, residuo = numero//base, numero%base
+        result = chars[residuo] + result
+    return result
